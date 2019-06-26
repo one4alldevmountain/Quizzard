b052b5ffb32668a3c8ab30439b508f6dce1cd8b3
Justus Ray <justusmray@gmail.com>
2019-06-25 19:18:16 -0600

    finished merge issues

--- src/Components/Auth/RegisterView.js
+++ src/Components/Auth/RegisterView.js
@@ -1,19 +1,14 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import axios from 'axios';
 import { updateUser } from '../../actions';
 import { connect } from 'react-redux';
 import { toast } from 'react-toastify';
-=======
-import React from 'react';
 import MaterialIcon from 'material-icons-react';
->>>>>>> master
 import { Link } from 'react-router-dom';
 import logo1 from '../images/logo1.png';
 import './Register.scss';
 
 
-<<<<<<< HEAD
 class Register extends Component{
 
 
@@ -70,10 +65,10 @@ class Register extends Component{
     render(){
         
         return (
-            <div>
+            <div className="register-form-parent-div">
                 <Link to="/">
                     <img
-                        className="logo"
+                        className="register-logo"
                         src={logo1}
                         alt="logo" />
                 </Link>
@@ -82,46 +77,58 @@ class Register extends Component{
                     <div className="auth-container">
     
     
-                            <section className="register-form">
+                            <section className="register-form-container">
 
-                                <form onSubmit={event => this.handleRegister(event)}>
+                                <form className="register-form" onSubmit={event => this.handleRegister(event)}>
+                                    <div className="input-section">
 
-                                    <div className="username">
-                                        <input
-                                            className="username-input"
-                                            placeholder="Username"
-                                            type="text"
-                                            onChange={event => this.handleInputChange(event.target.value, 'username')}
-                                        
-                                        />
+                                        <div className="username">
+                                            <div className="username-icon">
+                                               <MaterialIcon icon="person" color="white" />
+                                            </div>
+                                            <input
+                                                className="username-input"
+                                                placeholder="Username"
+                                                type="text"
+                                                onChange={event => this.handleInputChange(event.target.value, 'username')}
+                                            
+                                            />
+                                        </div>
+            
+                                        <div className="password">
+                                            <div className="password-icon">
+                                                 <MaterialIcon icon="vpn_key" color="white"/>
+                                            </div>
+            
+                                            <input
+                                                className="password-input"
+                                                placeholder="Password"
+                                                type="password"
+                                                onChange={event => this.handleInputChange(event.target.value, 'password')}
+                                            />
+                                        </div>
+                                        <div className="email">
+                                            <div className="email-icon">
+                                               <MaterialIcon icon="mail" color="white"/>
+                                            </div>
+                                            <input
+                                                className="email-input"
+                                                placeholder="Email Address"
+                                                type="text"
+                                                onChange={event => this.handleInputChange(event.target.value, 'email')}
+                                            />
+                                        </div>
+            
+                                        <div className="register-buttons">
+                                                <button className="register-button">Create account</button>
+                                            <p>or</p>
+            
+                                            <Link className="register-btn" to="/Login">
+                                                <p className="Login-text">Login</p>
+                                            </Link>
+                                        </div>
                                     </div>
-        
-                                    <div className="password">
-        
-                                        <input
-                                            className="password-input"
-                                            placeholder="Password"
-                                            type="password"
-                                            onChange={event => this.handleInputChange(event.target.value, 'password')}
-                                        />
-                                    </div>
-                                    <div className="email">
-                                        <input
-                                            className="email-input"
-                                            placeholder="Email Address"
-                                            type="text"
-                                            onChange={event => this.handleInputChange(event.target.value, 'email')}
-                                        />
-                                    </div>
-        
-                                    <div className="buttons">
-                                            <button className="register-button signup-text">Create account</button>
-                                        <p>or</p>
-        
-                                        <Link className="login-button" to="/Login">
-                                            <p className="Login-text">Login</p>
-                                        </Link>
-                                    </div>
+
                                 </form>
     
     
@@ -134,83 +141,6 @@ class Register extends Component{
             </div>
         );
     }
-=======
-
-const Register = (props) => {
-    return (
-        <div className="register-form-parent-div">
-            <section className="register-form-container">
-                <Link to="/">
-                    <img
-                        className="register-logo"
-                        src={logo1}
-                        alt="logo" />
-                </Link>
-
-                    <div className="register-form">
-
-
-                           <section className="input-section">
-                                        <div className="username">
-                                            <div className="username-icon">
-                                               <MaterialIcon icon="person" color="white" />
-                                            </div>
-                                            
-                                            <input
-                                                className="username-input"
-                                                placeholder="Username"
-                                                type="text"
-                                            
-                                            /> 
-                                        </div>
-
-                                        <div className="password">
-                                            <div className="password-icon">
-                                                 <MaterialIcon icon="vpn_key" color="white"/>
-                                            </div>
-                                           
-                                            <input
-                                                className="password-input"
-                                                placeholder="Password"
-                                                type="password"
-                                            />
-                                        </div>
-                                        <div className="email">
-                                            <div className="email-icon">
-                                               <MaterialIcon icon="mail" color="white"/>
-                                            </div>
-                                            
-                                            <input
-                                                className="email-input"
-                                                placeholder="Email Address"
-                                                type="text"
-                                            />
-                                        </div>
-                           </section>
-
-                     
-
-                                <div className="register-buttons">
-                                    <Link className="register-btn" to="/Register" >
-                                        sign up
-                                    </Link>
-
-                                    <Link className="register-btn" to="/Login">
-                                        Login
-                                    </Link>
-                                </div>
-
-                        
-
-
-                    </div>
-               
-            </section>
-           
-
-        </div>
-    );
->>>>>>> master
 }
 
 
